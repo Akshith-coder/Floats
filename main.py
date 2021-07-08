838663fb2b3c2a37524b1599da19156030026a42
AkshithKachaka1 <67799241+Akshith-coder@users.noreply.github.com>
2021-07-08 16:59:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Floats
+''''
+Floats are decimals in Python
+'''
+#Conversion of Integers and Strings to Floats
+''''
+the float() function to convert any number or string to a  decimal
+'''
+x = 3
+a = "3"
+print(float(x))
+print(float(a))
